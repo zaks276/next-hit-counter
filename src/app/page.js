0d5a305f58893a639deb29a0d69e7b3294fe2d2e
Mohammed Zakaria Khan <zaks276@gmail.com>
2026-02-01 18:36:12 +0200

    Exercise complete

--- src/app/page.js
+++ src/app/page.js
@@ -7,27 +7,14 @@ import {
 
 const DATABASE_PATH = '/src/database.json';
 
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
-
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+  hits += 1;
+  writeFile(DATABASE_PATH, JSON.stringify({hits}));
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {hits}</p>
     </main>
   );
 }
